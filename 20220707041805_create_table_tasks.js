24147f043a78aec6eb96e808e8f15b250c6a499f
ever7 <everton_ssouza@yahoo.com.br>
2022-07-17 21:28:48 -0300

    Add files via upload

---
+++ 20220707041805_create_table_tasks.js
@@ -0,0 +1,18 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('tasks', table => {
+        table.increments('id').primary()
+        table.string('desc').notNull()
+        table.dateTime('estimateAt')
+        table.dateTime('doneAt')
+        table.integer('userId').references('id')
+           .inTable('users').notNull()
+       })
+     };
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTable('tasks')
+};
